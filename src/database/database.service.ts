a75cd5cb56416ad40a50b9e58d51b8f6c9c9f80d
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2026-01-04 11:13:49 +0300

    feat: migrating the app into prisma 7

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,6 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '../../prisma/generated/prisma/client.js';
 
 @Injectable()
@@ -6,35 +8,47 @@ export class DatabaseService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  private readonly MAX_RETRIES = 8;
-  private readonly RETRY_DELAY_MS = 2000;
+  constructor(private readonly config: ConfigService) {
+    const connectionString = config.get<string>('DATABASE_URL');
+    if (!connectionString) {
+      throw new Error('DATABASE_URL environment variable is required');
+    }
+    const pool = new PrismaPg({ connectionString: connectionString });
+    if (!pool) {
+      throw new Error("Can't connect with the db");
+    }
+    super({ adapter: pool });
+  }
+  private MAX_RETRIES = 5;
+  private RETRIES = 0;
+  private INITIAL_RETRY_DELAY_MS = 1000;
 
   async onModuleInit() {
-    let retries = 0;
-    while (retries < this.MAX_RETRIES) {
+    while (this.RETRIES < this.MAX_RETRIES) {
       try {
         await this.$connect();
-        console.log('✅ Database connected successfully.');
+        console.log('✅ Connected to the database successfully!');
         return;
       } catch (err) {
-        retries++;
+        this.RETRIES++;
         console.error(
-          `❌ Database connection failed (attempt ${retries}):`,
-          err.message,
+          `❌ Database connection failed (attempt ${this.RETRIES}):`,
+          (err as Error).message,
         );
 
-        if (retries >= this.MAX_RETRIES) {
+        if (this.RETRIES >= this.MAX_RETRIES) {
           console.error('🚫 Could not connect to the database. Exiting now...');
           process.exit(1);
         }
 
-        console.log(`🔄 Retrying in ${this.RETRY_DELAY_MS / 1000} seconds...`);
-        await new Promise((resolve) =>
-          setTimeout(resolve, this.RETRY_DELAY_MS),
-        );
+        const delay =
+          this.INITIAL_RETRY_DELAY_MS * Math.pow(2, this.RETRIES - 1);
+        console.log(`🔄 Retrying in ${delay / 1000} seconds...`);
+        await new Promise((resolve) => setTimeout(resolve, delay));
       }
     }
   }
+
   async onModuleDestroy() {
     await this.$disconnect();
     console.log('🛑 Disconnected from the database');
